e8f84fd2fa83c9b8f1450ce58aeb29e8dd52493b
Ryan <rgulden@live.com>
2020-11-30 19:45:00 -0600

    Update setup_script.sh

--- setup_script.sh
+++ setup_script.sh
@@ -2,6 +2,9 @@
 cp /etc/secret-volume/.env /var/www/.env
 cp -r /var/www/public /tmp/www
 
+# set some perms
+chmod 777 -R /var/www/storage
+
 # install sql drivers
 docker-php-ext-install mysqli pdo pdo_mysql
 
