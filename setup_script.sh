7cce372c565fd2c158d92bf262e9c19ea1d96f97
Ryan <rgulden@live.com>
2020-11-30 19:20:40 -0600

    Create setup_script.sh

---
+++ setup_script.sh
@@ -0,0 +1,9 @@
+# copy files around
+cp /etc/secret-volume/.env /var/www/.env
+cp -r /var/www/public /tmp/www
+
+# install sql drivers
+docker-php-ext-install mysqli pdo pdo_mysql
+
+# setup database
+php artisan migrate
